f6e5fef305ec8d98ff171d18fbf0a0fa34d30bba
Hendriela <hendrik-heiser@gmx.de>
2020-10-29 18:48:08 +0100

    Added week 7 Genbank exercises

--- 6_strings.py
+++ 6_strings.py
@@ -13,15 +13,24 @@ target = riddle.partition('*****')[2].partition('#####')[0]
 for c in special:
     target = target.replace(c, '')
 
-
-s = 'IpdmtikuLIgSWmffamGyhsinrsyPOrXNrTVnhHdNbKYaiDxDkUZTgFAZKhJzyBuQXoE'
+#%% string formatting
+s = 'vTrXAXoWheCnXpDUclYfNPnSmFdZBanqCQcUFKayimnqTzMFjVxbpkrxLmvZDuYO'
 s1 = ''
 for i in range(0, len(s), 5):
+    print(i)
     if i % 10 == 0:
         s1 += s[i:i+5].upper()
     else:
         s1 += s[i:i+5].lower()
 print(s1+'{:10d}'.format(len(s1)))
+
+s = 'vTrXAXoWheCnXpDUclYfNPnSmFdZBanqCQcUFKayimnqTzMFjVxbpkrxLmvZDuYO'
+new = []
+for i in range(0, len(s), 10):
+    new.append(s[i:i+5].upper())
+    new.append(s[i+5:i+10].lower())
+s_new = ''.join(new)
+print(s_new+'{:10d}'.format(len(s_new)))
 
 #%% dicts
 
@@ -63,6 +72,17 @@ person['cervantes'] = ['Miguel de Cervantes',
                     '29 September 1547','23 April 1616']
 person['lincoln'] = ['Abraham Lincoln',
                     '12 February 1809','15 April 1865']
+
+d = {}  # has deaths as keys and long names as values
+
+
+for i in person:
+    for j in person:
+        death = person[i][2:]
+        name = person[i][0]
+        d[tuple(death)] = [name]
+print(d)
+
 
 death_dic = {}
 for key, value in person.items():
@@ -108,3 +128,104 @@ for codon, acid in cdn.items():
     else:
         aacid[ac].append(codon)
 print(aacid['glutamic acid'])
+
+
+########## Rewriting strings:
+
+# task:
+# from a given string:
+### make the first 5 chars uppercase, next 5 lower ...etc.
+### finally the string length is written with a width of 10 characters
+
+
+# s = 'EzHsPsKyTvKJKqoVzwRavQTzHDtHQyvhrFUIeTBRkSRpJPlLEocbyJBiRlngqmfUKcd'
+s = 'vTrXAXoWheCnXpDUclYfNPnSmFdZBanqCQcUFKayimnqTzMFjVxbpkrxLmvZDuYO'
+
+# Step 1.1 make list of lists which contains 5 elements each!
+a = 0
+b = 5
+
+ran = int((len(s) / 5)+5)
+# round up without package
+ran = int((len(s) / 5)+5) + int(len(s) % 5 > 0)
+# round up with package
+import math
+ran = math.ceil(len(s)/5)
+
+s_complete = ''
+for i in range(ran):
+    if i % 2 == 0:
+        s_complete += s[a:b].upper()
+    else:
+        s_complete += s[a:b].lower()
+    a += 5
+    b += 5
+
+print(s_complete+'{:10d}'.format(len(s_complete)))
+
+
+########## Rewriting strings:
+
+# task:
+# from a given string:
+### make the first 5 chars uppercase, next 5 lower ...etc.
+### finally the string length is written with a width of 10 characters
+
+
+# s = 'EzHsPsKyTvKJKqoVzwRavQTzHDtHQyvhrFUIeTBRkSRpJPlLEocbyJBiRlngqmfUKcd'
+s = 'vTrXAXoWheCnXpDUclYfNPnSmFdZBanqCQcUFKayimnqTzMFjVxbpkrxLmvZDu'
+
+# Step 1.1 make list of lists which contains 5 elements each!
+
+
+lyst_s2 = []
+a = 0
+b = 5
+
+# round up without package
+ran = int((len(s) / 5)+5) + int(len(s) % 5 > 0)
+# round up with package
+import math
+ran = math.ceil(len(s)/5)
+
+for i in range(ran):
+    lyst_s2.append(s[a:b])
+    a += 5
+    b += 5
+
+print(lyst_s2)
+
+# Step 2: iterate the list indexes to make them upper/lowercase
+
+
+lyst_s3 = []
+
+for i, element in enumerate(lyst_s2):
+    if i % 2 == 0:  # checks if index is even
+        lyst_s3.append(element.upper())
+    else:  # if i is odd
+        lyst_s3.append(element.lower())
+print(lyst_s3)
+
+# Step 2.1: now add all the elements from this list into one string again:
+
+
+s_complete = ''
+
+for i in lyst_s3:
+    s_complete += i
+
+print(s_complete)
+
+# Step 3: append a count number at the end with a number block of 10 -> 8 blankspaces
+
+
+count = 0
+for i in s_complete:
+    count += 1
+    print(i, count)
+c = str(count)
+
+### print with the count number of 10 char blocks:
+k = '{:10d}'.format(count)  # makes it a 10 char block
+print(s_complete + k)
